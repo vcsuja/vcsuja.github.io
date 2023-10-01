5fe9f7c12b6928a99e7437d52056545200149c77
Vivek C S <diabolicfreak@gmail.com>
2023-10-01 22:20:46 +0530

    fix: fix navbar

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,8 +4,8 @@
 	import { fly } from 'svelte/transition';
 </script>
 
-<nav class="border-b border-b-slate-600 fixed top-0 left-0 right-0 z-10 opacity-100 dark:bg-zinc-900 bg-white px-5">
-	<div class="container mx-auto flex items-center">
+<nav class="border-b border-b-slate-600 fixed top-0 left-0 right-0 z-10 opacity-100 dark:bg-zinc-900 bg-white">
+	<div class="container mx-auto flex items-center px-5">
 		<a href="/" class="text-lg">
 			<strong class="font-bold">Vineeth</strong> Chandran Suja
 		</a>
